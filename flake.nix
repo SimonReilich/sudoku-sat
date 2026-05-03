a7269fe3835caae55df17f2d77e9c233c763f8b6
SimonReilich <simon.reilich@proton.me>
2026-05-03 13:49:01 +0200

    removed other targets

--- flake.nix
+++ flake.nix
@@ -8,9 +8,6 @@
     let
       supportedSystems = [
         "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
       ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     in
